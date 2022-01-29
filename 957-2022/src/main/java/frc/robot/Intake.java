1ec9b40ce45850f6b73be13006c68d67bf990db9
caterbob <trickshotkoopa2007@gmail.com>
2022-01-29 10:38:04 -0800

    vision-integrated intake code

--- 957-2022/src/main/java/frc/robot/Intake.java
+++ 957-2022/src/main/java/frc/robot/Intake.java
@@ -1,8 +1,18 @@
 package frc.robot;
 
+import java.util.ArrayList;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -15,18 +25,61 @@ public class Intake {
     int var = 5;
     boolean lastCycle = false;
 
+    Rect r;
+    RedVision g = new RedVision();
+    
+    int centerX = 0;
+    int centerY = 0;
 
-    public void ExtendCyl() {
+    public int getX(int target) {
+        r = Imgproc.boundingRect(g.filterContoursOutput().get(target));
+        centerX = r.x + (r.width / 2);
+        return centerX;
+    }
+
+    public int getY(int target) {
+        r = Imgproc.boundingRect(g.filterContoursOutput().get(target));
+        centerY = r.y + (r.height / 2);
+        return centerY;
+    }
+
+
+    public Intake(){
+
+        new Thread(() -> {
+            CameraServer.startAutomaticCapture();
+            CvSink sink;
+            CvSource outputStream = CameraServer.putVideo("Blur", 320, 240); // Remove this for competitions
+            
+            Mat mat = new Mat();
+            sink = CameraServer.getVideo();
+      
+      
+            while(!Thread.interrupted()) {
+      
+              if (sink.grabFrame(mat) == 0) {
+                continue;
+              }
+              
+              g.process(mat);
+              outputStream.putFrame(g.hsvThresholdOutput);  // Remove this for competition (expensive operation)
+      
+            }
+          }).start();
+
+    }
+
+    public void extendCyl() {
         doubleSolenoid.set(Value.kForward);
         intakeMotor.set(1);
     }
 
-    public void RetractCyl() {
+    public void retractCyl() {
         doubleSolenoid.set(Value.kReverse);
         intakeMotor.set(0);
     }
 
-    public int run(int cargoNum, boolean button) {
+    public int cycle(int cargoNum, boolean button) {
     
         switch (var) {
             case 0:
@@ -40,7 +93,7 @@ public class Intake {
             break;
 
             case 2:
-                ExtendCyl();
+                extendCyl();
                 var = 3;
             break;
 
@@ -55,7 +108,7 @@ public class Intake {
             break;
 
             case 5:
-                RetractCyl();
+                retractCyl();
                 var = 0;
             break;
 
@@ -67,6 +120,9 @@ public class Intake {
             if (lastCycle == true)
                 cargoNum++;
         }
+
+        if (cargoNum > 1 && var != 0) 
+            var = 5;
 
         lastCycle = nowCycle;
 
