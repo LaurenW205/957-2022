2044760854354d6902a82a0b5613e1b0e4fd973a
DESKTOP-648PQJR\JENJO <lauren.ward486@gmail.com>
2022-02-05 10:04:28 -0800

    auto 2

--- 957-2022/src/main/java/frc/robot/Robot.java
+++ 957-2022/src/main/java/frc/robot/Robot.java
@@ -63,47 +63,6 @@ public class Robot extends TimedRobot {
       }
 
       break;
-<<<<<<< HEAD
-      case 2:
-
-      switch(m_autoStep){
-       
-       
-        case 0:
-         m_drivetrain.driveStraight(46, 0, 0.5);
-         m_autoStep ++;
-        break;
-
-        case 1:
-         //intake cargo
-         m_autoStep ++;
-        break;
-
-        case 2:
-         m_drivetrain.driveStraight(24, 0, 0.5);
-         m_autoStep ++;
-        break;
-
-        case 3:
-         //m_turret.run(true);
-         m_autoStep ++;
-        break;
-
-        case 4:
-        //shoot
-         m_autoStep ++;
-        break;
-
-        case 5:
-         m_drivetrain.turnTo(45);
-         m_autoStep ++;
-        break;
-
-
-
-
-      }
-=======
 
       case 1: 
       
@@ -158,7 +117,6 @@ public class Robot extends TimedRobot {
       
      
 
->>>>>>> 53f1d3cbbef84e4b41eb127a7bdbe06d3a00140e
     }
     
   }
